d9a3395d41388e87fac55b472e5ac7d02cc6942e
Thoriqulhaq Jibril <thoriqulhaqjibril1@gmail.com>
2022-04-23 22:51:34 +0700

    Add dependency for .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:elibrary_app/screens/studentViews/registerScreen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
