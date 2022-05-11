799eddf6dce66d82b1b03f3b48073e4ec8856c48
Muhammad Darlen Sava <86015798+NostrumPyre@users.noreply.github.com>
2022-05-11 22:06:39 +0800

    Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:elibrary_app/screens/studentViews/editProfile.dart';
 import 'package:flutter/material.dart';
 import 'package:elibrary_app/screens/studentViews/registerScreen.dart';
+import 'package:elibrary_app/screens/studentViews/loginScreen.dart';
 import 'package:elibrary_app/screens/studentViews/forgotPassword.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: forgotPassword(),
+      home: loginScreen(),
     );
   }
 }
