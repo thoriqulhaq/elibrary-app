0445109b84eb1b4c5b6e0c2b42e40da276fca527
Thoriqulhaq Jibril <thoriqulhaqjibril1@gmail.com>
2022-05-25 09:57:43 +0700

    Implement Search Content Function Add HomeWrapper UI

---
+++ lib/screens/lecturerViews/homeWrapperScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeWrapperScreen extends StatefulWidget {
+  HomeWrapperScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeWrapperScreen> createState() => _HomeWrapperScreenState();
+}
+
+class _HomeWrapperScreenState extends State<HomeWrapperScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/studentViews/searchContentScreen.dart
+++ lib/screens/studentViews/searchContentScreen.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firestore_search/firestore_search.dart';
 
 class searchContentScreen extends StatefulWidget {
   searchContentScreen({Key? key}) : super(key: key);
+
+  get indexedField => null;
 
   @override
   State<searchContentScreen> createState() => _searchContentScreenState();
@@ -10,67 +14,77 @@ class searchContentScreen extends StatefulWidget {
 class _searchContentScreenState extends State<searchContentScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        title: Container(
-          width: double.infinity,
-          height: 40,
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(5)),
-          child: Center(
-            child: TextField(
-              // expands: true,
-              // maxLines: null,
-              style: TextStyle(fontSize: 20.0, color: Colors.black54),
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    print('Search Process');
-                  },
-                ),
-                hintText: 'Search...',
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.fromLTRB(12, 7, 12, 3),
-              ),
-            ),
-          ),
-        ),
-        // actions: <Widget>[
-        //   IconButton(
-        //     icon: const Icon(Icons.navigate_next),
-        //     tooltip: 'Go to the next page',
-        //     onPressed: () {
-        //       Navigator.push(context, MaterialPageRoute<void>(
-        //         builder: (BuildContext context) {
-        //           return Scaffold(
-        //             appBar: AppBar(
-        //               title: const Text('Next page'),
-        //             ),
-        //             body: const Center(
-        //               child: Text(
-        //                 'This is the next page',
-        //                 style: TextStyle(fontSize: 24),
-        //               ),
-        //             ),
-        //           );
-        //         },
-        //       ));
-        //     },
-        //   ),
-        // ],
-      ),
-      body: const Center(
-        child: Text(
-          'Not Found',
-          style: TextStyle(fontSize: 24.0, color: Colors.black54),
-        ),
-      ),
+    return FirestoreSearchScaffold(
+      firestoreCollectionName: 'contents',
+      searchBy: 'title',
+      scaffoldBody: Center(),
+      dataListFromSnapshot: DataModel().dataListFromSnapshot,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final List<DataModel>? dataList = snapshot.data;
+          if (dataList!.isEmpty) {
+            return const Center(
+              child: Text('No Results Returned'),
+            );
+          }
+          return ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                final DataModel data = dataList[index];
+
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        '${data.title}',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          bottom: 8.0, left: 8.0, right: 8.0),
+                      child: Text('${data.author}',
+                          style: Theme.of(context).textTheme.bodyText1),
+                    )
+                  ],
+                );
+              });
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: Text('No Results Returned'),
+            );
+          }
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
+  }
+}
+
+class DataModel {
+  final String? title;
+  final String? author;
+
+  DataModel({this.title, this.author});
+
+  //Create a method to convert QuerySnapshot from Cloud Firestore to a list of objects of this DataModel
+  //This function in essential to the working of FirestoreSearchScaffold
+
+  List<DataModel> dataListFromSnapshot(QuerySnapshot querySnapshot) {
+    return querySnapshot.docs.map((snapshot) {
+      final Map<String, dynamic> dataMap =
+          snapshot.data() as Map<String, dynamic>;
+
+      return DataModel(title: dataMap['title'], author: dataMap['author']);
+    }).toList();
   }
 }
