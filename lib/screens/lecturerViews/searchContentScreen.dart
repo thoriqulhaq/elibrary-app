aa55ed46ec3f4dca32aa3ee02c98ee518216ba02
thoriqulhaq <thoriqulhaqjibril1@gmail.com>
2022-06-26 13:43:23 +0700

    Download Content (Lecturer)

--- lib/screens/lecturerViews/searchContentScreen.dart
+++ lib/screens/lecturerViews/searchContentScreen.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:elibrary_app/screens/lecturerViews/editContent.dart';
+import 'package:elibrary_app/screens/studentViews/book_detail.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firestore_search/firestore_search.dart';
+import 'package:share_plus/share_plus.dart';
 
 class searchContentScreen extends StatefulWidget {
   searchContentScreen({Key? key}) : super(key: key);
@@ -37,11 +39,39 @@ class _searchContentScreenState extends State<searchContentScreen> {
 
                 return ListTile(
                   leading: Image.network('${data.cover}'),
-                  title: Text('${data.title}'),
+                  title: new GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => BookDetail(
+                            titleBook: data.title!,
+                            descBook: data.desc!,
+                            coverUrl: data.cover!,
+                            downloadUrl: data.url!,
+                            author: data.author!,
+                            bookId: data.id!,
+                          ),
+                        ),
+                      );
+                    },
+                    child: new Text('${data.title}'),
+                  ),
                   subtitle: Text('${data.author}'),
                   trailing: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
+                      IconButton(
+                        onPressed: () async {
+                          final ShareUrl = data.url;
+                          final authorName = data.author;
+                          final bookName = data.title;
+                          await Share.share(
+                              'check out the book $bookName by $authorName! \n\n$ShareUrl');
+                          ;
+                        },
+                        icon: Icon(Icons.share_sharp),
+                      ),
                       IconButton(
                         icon: Icon(Icons.edit),
                         onPressed: () {
@@ -103,8 +133,11 @@ class DataModel {
   final String? title;
   final String? author;
   final String? cover;
+  final String? url;
+  final String? desc;
   final String? id;
-  DataModel({this.title, this.author, this.cover, this.id});
+  DataModel(
+      {this.title, this.author, this.cover, this.url, this.desc, this.id});
 
   //Create a method to convert QuerySnapshot from Cloud Firestore to a list of objects of this DataModel
   //This function in essential to the working of FirestoreSearchScaffold
@@ -118,6 +151,8 @@ class DataModel {
           title: dataMap['title'],
           author: dataMap['author'],
           cover: dataMap['cover'],
+          url: dataMap['url'],
+          desc: dataMap['desc'],
           id: snapshot.id);
     }).toList();
   }
